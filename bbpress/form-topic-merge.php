5973c88b71d1b56df190dfd62559d94aa6ac36c6
Peter Davenport <peter@housedavenport.com>
2014-02-24 17:28:05 -0500

    Update form-topic-merge.php

--- bbpress/form-topic-merge.php
+++ bbpress/form-topic-merge.php
@@ -19,25 +19,25 @@
 
 				<fieldset class="bbp-form">
 
-					<legend><?php printf( __( 'Merge topic "%s"', 'bbpress' ), bbp_get_topic_title() ); ?></legend>
+					<legend><?php printf( __( 'Merge topic "%s"', 'stargazer-colloquium' ), bbp_get_topic_title() ); ?></legend>
 
 					<div>
 
 						<div class="bbp-template-notice info">
-							<p><?php _e( 'Select the topic to merge this one into. The destination topic will remain the lead topic, and this one will change into a reply.', 'bbpress' ); ?></p>
-							<p><?php _e( 'To keep this topic as the lead, go to the other topic and use the merge tool from there instead.', 'bbpress' ); ?></p>
+							<p><?php _e( 'Select the topic to merge this one into. The destination topic will remain the lead topic, and this one will change into a reply.', 'stargazer-colloquium' ); ?></p>
+							<p><?php _e( 'To keep this topic as the lead, go to the other topic and use the merge tool from there instead.', 'stargazer-colloquium' ); ?></p>
 						</div>
 
 						<div class="bbp-template-notice">
-							<p><?php _e( 'All replies within both topics will be merged chronologically. The order of the merged replies is based on the time and date they were posted. If the destination topic was created after this one, it\'s post date will be updated to second earlier than this one.', 'bbpress' ); ?></p>
+							<p><?php _e( 'All replies within both topics will be merged chronologically. The order of the merged replies is based on the time and date they were posted. If the destination topic was created after this one, it\'s post date will be updated to second earlier than this one.', 'stargazer-colloquium' ); ?></p>
 						</div>
 
 						<fieldset class="bbp-form">
-							<legend><?php _e( 'Destination', 'bbpress' ); ?></legend>
+							<legend><?php _e( 'Destination', 'stargazer-colloquium' ); ?></legend>
 							<div>
 								<?php if ( bbp_has_topics( array( 'show_stickies' => false, 'post_parent' => bbp_get_topic_forum_id( bbp_get_topic_id() ), 'post__not_in' => array( bbp_get_topic_id() ) ) ) ) : ?>
 
-									<label for="bbp_destination_topic"><?php _e( 'Merge with this topic:', 'bbpress' ); ?></label>
+									<label for="bbp_destination_topic"><?php _e( 'Merge with this topic:', 'stargazer-colloquium' ); ?></label>
 
 									<?php
 										bbp_dropdown( array(
@@ -51,7 +51,7 @@
 
 								<?php else : ?>
 
-									<label><?php _e( 'There are no other topics in this forum to merge with.', 'bbpress' ); ?></label>
+									<label><?php _e( 'There are no other topics in this forum to merge with.', 'stargazer-colloquium' ); ?></label>
 
 								<?php endif; ?>
 
@@ -59,24 +59,24 @@
 						</fieldset>
 
 						<fieldset class="bbp-form">
-							<legend><?php _e( 'Topic Extras', 'bbpress' ); ?></legend>
+							<legend><?php _e( 'Topic Extras', 'stargazer-colloquium' ); ?></legend>
 
 							<div>
 
 								<?php if ( bbp_is_subscriptions_active() ) : ?>
 
 									<input name="bbp_topic_subscribers" id="bbp_topic_subscribers" type="checkbox" value="1" checked="checked" tabindex="<?php bbp_tab_index(); ?>" />
-									<label for="bbp_topic_subscribers"><?php _e( 'Merge topic subscribers', 'bbpress' ); ?></label><br />
+									<label for="bbp_topic_subscribers"><?php _e( 'Merge topic subscribers', 'stargazer-colloquium' ); ?></label><br />
 
 								<?php endif; ?>
 
 								<input name="bbp_topic_favoriters" id="bbp_topic_favoriters" type="checkbox" value="1" checked="checked" tabindex="<?php bbp_tab_index(); ?>" />
-								<label for="bbp_topic_favoriters"><?php _e( 'Merge topic favoriters', 'bbpress' ); ?></label><br />
+								<label for="bbp_topic_favoriters"><?php _e( 'Merge topic favoriters', 'stargazer-colloquium' ); ?></label><br />
 
 								<?php if ( bbp_allow_topic_tags() ) : ?>
 
 									<input name="bbp_topic_tags" id="bbp_topic_tags" type="checkbox" value="1" checked="checked" tabindex="<?php bbp_tab_index(); ?>" />
-									<label for="bbp_topic_tags"><?php _e( 'Merge topic tags', 'bbpress' ); ?></label><br />
+									<label for="bbp_topic_tags"><?php _e( 'Merge topic tags', 'stargazer-colloquium' ); ?></label><br />
 
 								<?php endif; ?>
 
@@ -84,11 +84,11 @@
 						</fieldset>
 
 						<div class="bbp-template-notice error">
-							<p><?php _e( '<strong>WARNING:</strong> This process cannot be undone.', 'bbpress' ); ?></p>
+							<p><?php _e( '<strong>WARNING:</strong> This process cannot be undone.', 'stargazer-colloquium' ); ?></p>
 						</div>
 
 						<div class="bbp-submit-wrapper">
-							<button type="submit" tabindex="<?php bbp_tab_index(); ?>" id="bbp_merge_topic_submit" name="bbp_merge_topic_submit" class="button submit"><?php _e( 'Submit', 'bbpress' ); ?></button>
+							<button type="submit" tabindex="<?php bbp_tab_index(); ?>" id="bbp_merge_topic_submit" name="bbp_merge_topic_submit" class="button submit"><?php _e( 'Submit', 'stargazer-colloquium' ); ?></button>
 						</div>
 					</div>
 
@@ -101,7 +101,7 @@
 	<?php else : ?>
 
 		<div id="no-topic-<?php bbp_topic_id(); ?>" class="bbp-no-topic">
-			<div class="entry-content"><?php is_user_logged_in() ? _e( 'You do not have the permissions to edit this topic!', 'bbpress' ) : _e( 'You cannot edit this topic.', 'bbpress' ); ?></div>
+			<div class="entry-content"><?php is_user_logged_in() ? _e( 'You do not have the permissions to edit this topic!', 'stargazer-colloquium' ) : _e( 'You cannot edit this topic.', 'stargazer-colloquium' ); ?></div>
 		</div>
 
 	<?php endif; ?>
