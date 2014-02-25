9f4ec9f3f0ea390d8bf11012745537cd0c370984
Peter Davenport <peter@housedavenport.com>
2014-02-24 19:05:43 -0500

    added after_theme_setup to do things correctly

--- functions.php
+++ functions.php
@@ -1,17 +1,25 @@
 <?php
 
-/* Uses the custom color set in Stargazer within BBPress */
-add_action( 'wp_head', 'bbpress_custom_color' ); 
 
-    function bbpress_custom_color() {
+add_action( 'after_setup_theme', 'colloquium_theme_setup');
+
+function colloquium_theme_setup() {
+
+    /* Uses the custom color set in Stargazer within BBPress */
+    add_action( 'wp_head', 'bbpress_custom_color' ); 
+
+}
+
+
+function bbpress_custom_color() {
         
     	$style = '';
     	$hex = get_theme_mod( 'color_primary', '' );
     	$rgb = join( ', ', hybrid_hex_to_rgb( $hex ) );
     	$style .= "li.bbp-topic-title .font-headlines a.bbp-topic-permalink:hover, a.bbp-forum-title.entry-title:hover { color: #{$hex}; } ";
-    	$style .= "{ background-color: rgba( {$rgb}, 0.8 ); } ";
-    	$style .= "#bbpress-forums li.bbp-header, #bbpress-forums fieldset.bbp-form legend { background-color: #{$hex}; }";
-    	$style .= "#bbpress-forums li.bbp-footer { border-top-color: #{$hex}; } ";
+    	$style .= "span.bbp-admin-links a.bbp-reply-to-link, span.bbp-admin-links a.bbp-topic-reply-link { background-color: rgba( {$rgb}, 0.8 ); } ";
+    	$style .= "#bbpress-forums li.bbp-header, #bbpress-forums fieldset.bbp-form legend, span.bbp-admin-links a.bbp-reply-to-link:hover, span.bbp-admin-links a.bbp-topic-reply-link:hover { background-color: #{$hex}; }";
+    	$style .= " { border-top-color: #{$hex}; } ";
     	$style = "\n" . '<style type="text/css" id="bbpress-custom-color">' . trim( $style ) . '</style>' . "\n";
     	echo $style;
 }
